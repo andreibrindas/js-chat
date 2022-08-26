3a341ecdcd88b7a28e17fe2fd3c64407c6f9d365
Andrei Brindas <abrindas@healthfirst.org>
2022-08-26 11:24:43 +0300

    updated user base

--- server/server.js
+++ server/server.js
@@ -1,1 +1,21 @@
-console.log("hello world");
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const connectDB = require("./config/db");
+const { errorHandler } = require("./middleware/errorMiddleware");
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extendedd: false }));
+
+// add the user routes
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
+
+// start the server app
+app.listen(port, () => console.log(`Server started on port ${port}`));
